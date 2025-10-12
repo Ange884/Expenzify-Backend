d6a79dd64e15b63d0d051c4d09b8bf4a7ccaff3c
Ange <ishimwenzizaangell@gmail.com>
2025-10-12 22:34:13 +0200

    .

--- Server.js
+++ Server.js
@@ -1,17 +1,35 @@
 //const express = require ("express");
 import express from "express";
 import dotenv from "dotenv";
+import { sql } from "./config/db";
 dotenv.config();
 
 const app = express();
 
 const PORT = process.env.PORT ||  5001  ;  
-connectDB(process.env.DATABASE_URL)
+async function initDB(){
+    try{
+        await sql`CREATE TABLE IF NOT EXISTS transactions(
+        id SERIAL PRIMARY KEY,
+        user_id VARCHAR (255) NOT NULL,
+        title VARCHAR(255) NOT NULL,
+        amount DECIMAL(10,2) NOT NULL,
+        category VARCHAR(255) NOT NULL,
+        created at DATE NOT NULL DEFAULT CURRENT_DATE
+        )`
+        console.log("Database initialized successfully");
+    }catch (error){
+          console.log("Error initializing DB", error)
+          process.exit(1);
+    }
+}
+
 app.get("/", (req,res) =>{
     res.send("it is working 123");
 });
-console.log("my port:", process.env.PORT);
 
-app.listen(5001,() =>{
+initDB().then(() =>{
+    app.listen(5001,() =>{
     console.log("server is up and running on PORT:5001")
-})
+  });
+});
